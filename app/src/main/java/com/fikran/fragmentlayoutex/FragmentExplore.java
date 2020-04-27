9c9de130adac2f01c070e6bb10c9f542a2e2ed6c
fikran-dev <fikranwaleng@gmail.com>
2020-04-27 11:16:15 +0800

    pes

---
+++ app/src/main/java/com/fikran/fragmentlayoutex/FragmentExplore.java
@@ -0,0 +1,21 @@
+package com.fikran.fragmentlayoutex;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentExplore extends Fragment {
+    View view;
+    public FragmentExplore() {
+    }
+
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.explore_fragment,container,false);
+        return view;
+    }
+}
+
